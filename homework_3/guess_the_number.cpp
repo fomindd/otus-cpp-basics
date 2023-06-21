69512bc5abcf7211b57068d54980eab5a6839dec
Daniil Fomin <122152800+fomindd@users.noreply.github.com>
2023-06-21 16:36:19 +0700

    Create guess_the_number.cpp

---
+++ homework_3/guess_the_number.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+#include <fstream>
+#include <string>
+#include <cstdlib>
+#include <ctime>
+#include <check_value.h>
+#include <random_value.h>
+#include <high_scores.h>
+
+const int max_value = 100;
+std::string set_name();
+
+int main()
+{
+    int random_value_from_func = random_value(max_value);
+    std::string name =  set_name();
+    int attempts = check_value(random_value_from_func);
+    write_data(name, attempts);
+    high_score();
+
+    return 0;
+}
+
+std::string set_name()
+{
+    std::cout << "Hi! Enter your name, please:" << std::endl;
+    std::string user_name;
+    std::cin >> user_name;
+    return user_name;
+}
